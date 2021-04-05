aab02da356f82a5a4600ace70e672d7f1562e82c
MyMac <mymac@Joels-MacBook-Pro.local>
2021-04-05 15:29:08 -0500

    first commit

---
+++ PythagoreanTest.java
@@ -0,0 +1,8 @@
+
+public class PythagoreanTest{
+    public static void main(String[] args){
+        Pythagorean iD = new Pythagorean();
+        double result = iD.calculateHypotenuse(4, 2);
+        System.out.println(result);
+    }
+}
